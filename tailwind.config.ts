c5c455647fb64524cffff26de99ba1e774c0f800
jamesatomc <jiraphonwansai2002@gmail.com>
2024-04-02 17:23:06 +0700

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+      },
+      maxHeight: {
+        '96': '24rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation : {
+        'spin-slow-30': 'spin 30s linear infinite',
+        'spin-slow-25': 'spin 25s linear infinite',
+        'spin-slow-10': 'spin 10s linear infinite',
+        'marquee-infinite' : 'marquee 25s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
