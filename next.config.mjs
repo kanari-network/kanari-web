4bd23c58926285c829672fae4ee35ac040c87baf
jamesatomc <jiraphonwansai2002@gmail.com>
2025-01-14 16:44:37 +0700

    Configure Next.js settings: enable unoptimized images, set output to export, enforce trailing slashes, and activate strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  output: 'export',
+  trailingSlash: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
