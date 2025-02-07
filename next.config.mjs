eb1ec5a6111990e43c02223f0dba679d41c99a31
jamesatomc <jiraphonwansai2002@gmail.com>
2025-02-07 16:46:01 +0700

    Remove Strict-Transport-Security header from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -12,19 +12,6 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
